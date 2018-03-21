433acb01c99ed4fc658ccbd2affb3fa5cfc6a18c
Olexa Bilaniuk <obilaniu@gmail.com>
2018-03-21 06:06:21 -0400

    Move argument-parsing Actions to separate module nauka.ap.*
    Also, rename the actions to make their name shorter and remove
    redundancies.

--- src/nauka/__init__.py
+++ src/nauka/__init__.py
@@ -1,15 +1,9 @@
 # -*- coding: utf-8 -*-
-
 #
 # We early-import __version__ so that all submodules can see this information
 # if needed.
 #
-from .version import version as __version__
-from .        import version
-
-#
 # Other packages follow.
 #
-from .        import exp
-from .        import utils
-from .exp     import Experiment
+from .version import version as __version__
+from .        import ap, exp, utils, version
